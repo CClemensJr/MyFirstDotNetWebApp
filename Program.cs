5710dffe68b36fc519908b69418076ca2faee0c9
Charles Clemens <cclemensjr@gmail.com>
2017-10-14 20:48:02 -0700

    A simple web app using the DotNet framework

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using movingtotheweb;
+
+namespace supermva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+              .UseKestrel()
+              .UseStartup<Startup>()
+              .Build();
+
+            host.Run();
+        }
+    }
+}
